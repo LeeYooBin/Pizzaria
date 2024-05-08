ac31db6186ccfb159cb21a1d5ea0e6ae395c1611
LeeYooBin <santosroberdan25@gmail.com>
2024-05-07 23:37:48 -0300

    Style: Adjusting the style of Footer

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -3,10 +3,12 @@ import Link from "next/link";
 import PizzaIcon from "./pizza-icon";
 
 const Logo = () => (
-  <Link className="flex items-center gap-2" href="/">
-    <PizzaIcon className="h-16 w-16" />
-    <span className="text-3xl font-bold">Slice of Heaven</span>
-  </Link>
+  <div className="flex items-center space-x-4">
+    <Link className="flex items-center space-x-2" href="/">
+      <PizzaIcon className="h-14 w-14" />
+      <span className="text-3xl font-semibold">Pizza</span>
+    </Link>
+  </div>
 );
 
 export default Logo;
