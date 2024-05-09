25181215df6a197c3fbb077e17735eaee93af6f1
LeeYooBin <santosroberdan25@gmail.com>
2024-05-08 23:42:15 -0300

    Fix: Adjusting API reqs

--- src/data/utils/api.ts
+++ src/data/utils/api.ts
@@ -7,36 +7,64 @@ const api = axios.create({
 
 export default api;
 
-export const registerUser = async (userData: UserData) => {
+export const registerUser = async ({ name, email, password }: UserData) => {
   try {
-    const response = await api.post("user/register", userData);
+    const response = await api.post(`user/register`, { name, email, password });
     return response.data;
   } catch (error) {
     throw new Error("Failed to register user");
   }
 };
 
-export const addToCart = async (productId: string) => {
+export const addToCart = async (
+  productId: string,
+  userId: string,
+  token: string
+) => {
   try {
-    const response = await api.post(`user/cart/add/${productId}`);
+    const response = await api.post(
+      `user/cart/add/${userId}`,
+      { productId },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    );
     return response.data;
   } catch (error) {
     throw new Error("Failed to add product to cart");
   }
 };
 
-export const createOrder = async (productId: string) => {
+export const createOrder = async (
+  productId: string,
+  userId: string,
+  token: string
+) => {
   try {
-    const response = await api.post(`user/order/create/${productId}`);
+    const response = await api.post(
+      `user/order/create/${userId}`,
+      { productId },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    );
     return response.data;
   } catch (error) {
     throw new Error("Failed to create order");
   }
 };
 
-export const getOrders = async () => {
+export const getOrders = async (userId: string, token: string) => {
   try {
-    const response = await api.get("user/order/:id");
+    const response = await api.get(`user/order/${userId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
     return response.data;
   } catch (error) {
     throw new Error("Failed to get orders");
@@ -45,28 +73,49 @@ export const getOrders = async () => {
 
 export const updateUser = async (
   userId: string,
-  userData: Partial<UserData>
+  userData: Partial<UserData>,
+  token: string
 ) => {
   try {
-    const response = await api.put(`user/update/${userId}`, userData);
+    const response = await api.put(`user/update/${userId}`, userData, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
     return response.data;
   } catch (error) {
     throw new Error("Failed to update user");
   }
 };
 
-export const deleteUser = async (userId: string) => {
+export const deleteUser = async (userId: string, token: string) => {
   try {
-    const response = await api.delete(`user/delete/${userId}`);
+    const response = await api.delete(`user/delete/${userId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
     return response.data;
   } catch (error) {
     throw new Error("Failed to delete user");
   }
 };
 
-export const removeFromCart = async (productId: string) => {
+export const removeFromCart = async (
+  productId: string,
+  userId: string,
+  token: string
+) => {
   try {
-    const response = await api.delete(`user/cart/remove/${productId}`);
+    const response = await api.post(
+      `user/cart/remove/${userId}`,
+      { productId },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    );
     return response.data;
   } catch (error) {
     throw new Error("Failed to remove product from cart");
